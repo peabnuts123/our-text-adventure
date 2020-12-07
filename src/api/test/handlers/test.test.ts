469773c291c408624804bdceecc8785e39589047
peabnuts123 <peabnuts123@gmail.com>
2020-12-07 19:29:50 +1300

    [API] Add simple test for test handler

--- src/api/test/handlers/test.test.ts
+++ src/api/test/handlers/test.test.ts
@@ -1,19 +1,39 @@
 import { handler } from '@app/handlers/test';
+import GameScreen from '@app/db/models/GameScreen';
+import Command from '@app/db/models/Command';
 
 import SimpleRequest from '@test/local/util/SimpleRequest';
 import { invokeHandler } from '@test/util/invoke-handler';
+import MockDb from '@test/mocks/mockDb';
 
 describe('Test Handler', () => {
-  test('Returns a result', async () => {
+  beforeEach(() => {
+    MockDb.reset();
+  });
+
+  test('returns all screen data', async () => {
     // Setup
+    const mockScreens: GameScreen[] = [
+      new GameScreen('d9ba40f7-cc19-485b-88c9-43aae7fd32d4', ["Test", "body", "A"], []),
+      new GameScreen('bcfc72df-4e16-4ef6-b41f-961edcbdf729', ["Test", "body", "B"], [
+        new Command('2ae7dc7e-a766-401e-9200-a4a0060d3579', 'look bone', 'd9ba40f7-cc19-485b-88c9-43aae7fd32d4'),
+      ]),
+    ];
     const mockRequest: SimpleRequest = {
-      path: "/test/123465",
+      path: "/test/123456",
     };
+
+    MockDb.screens = mockScreens;
 
     // Test
     const response = await invokeHandler(handler, mockRequest);
 
     // Assert
-    expect(response).not.toBeNull();
+    expect(response.statusCode).toBe(200);
+    expect(response.headers).toBeDefined();
+    expect(response.headers && response.headers['Content-Type']).toBe('application/json');
+    expect(response.body && JSON.parse(response.body)).toEqual({
+      screens: mockScreens,
+    });
   });
 });
--- src/api/test/util/invoke-handler.ts
+++ src/api/test/util/invoke-handler.ts
@@ -1,13 +1,13 @@
-import { APIGatewayProxyHandlerV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { APIGatewayProxyHandlerV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 
 import SimpleRequest from '@test/local/util/SimpleRequest';
 import getMockContext, { ContextOverrides } from '@test/local/util/get-mock-context';
 import getMockProxyRequest from '@test/local/util/get-mock-proxy-request';
 
-export function invokeHandler(handler: APIGatewayProxyHandlerV2, request: SimpleRequest, contextOverrides?: ContextOverrides): Promise<APIGatewayProxyResultV2> {
+export function invokeHandler(handler: APIGatewayProxyHandlerV2, request: SimpleRequest, contextOverrides?: ContextOverrides): Promise<APIGatewayProxyStructuredResultV2> {
   return handler(
     getMockProxyRequest(request),
     getMockContext(contextOverrides),
     () => { },
-  ) as Promise<APIGatewayProxyResultV2>;
+  ) as Promise<APIGatewayProxyStructuredResultV2>;
 }
