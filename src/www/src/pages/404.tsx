e7c002a405ecee0e9047a7d424b8adc238973a30
peabnuts123 <peabnuts123@gmail.com>
2020-12-14 11:03:51 +1300

    [WWW] Add 404/Not found page

---
+++ src/www/src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+
+
+const NotFound: FunctionComponent = () => {
+  return (
+    <>
+      <h1>Not found</h1>
+      <p>The page you are looking for was not found. Sorry &apos;bout it. Insert funny 404 gag here.</p>
+      <p>Click <a href="/">here</a> to return to the home page.</p>
+    </>
+  );
+};
+
+export default NotFound;
