152b33ebd42f479783985fa1d581dc61a2ca4ae3
peabnuts123 <peabnuts123@gmail.com>
2021-01-12 10:42:01 +1300

    [WWW] Remove references to a screen's commands

--- src/www/src/models/GameScreen.ts
+++ src/www/src/models/GameScreen.ts
@@ -1,29 +1,24 @@
 import ApiModel from "./base/ApiModel";
-import Command, { CommandDto } from "./Command";
 
 export interface GameScreenDto {
   id: string;
   body: string[];
-  commands: CommandDto[];
 }
 
 class GameScreen extends ApiModel<GameScreenDto> {
   public readonly id: string;
   public readonly body: string[];
-  public readonly commands: Command[];
 
   public constructor(dto: GameScreenDto) {
     super();
     this.id = dto.id;
     this.body = dto.body;
-    this.commands = dto.commands.map((dto) => new Command(dto));
   }
 
   public toDto(): GameScreenDto {
     return {
       id: this.id,
       body: this.body,
-      commands: this.commands.map((command) => command.toDto()),
     };
   }
 }
